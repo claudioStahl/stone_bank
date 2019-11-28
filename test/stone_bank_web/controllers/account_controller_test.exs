84fee3903aa0ef7f786f0a9bdff0b2026c2ca7bd
claudiostahl <claudioarosseto@gmail.com>
2019-11-27 23:10:26 -0200

    Update account controller test with fixtures

--- test/stone_bank_web/controllers/account_controller_test.exs
+++ test/stone_bank_web/controllers/account_controller_test.exs
@@ -2,39 +2,14 @@ defmodule StoneBankWeb.AccountControllerTest do
   use StoneBankWeb.ConnCase
 
   import Mox
+  import StoneBank.Fixtures.Accounts
 
+  alias Ecto.Changeset
   alias StoneBank.AccountsMock
   alias StoneBank.Accounts.Account
 
   @create_attrs %{name: "some name", password: "some_password"}
   @invalid_attrs %{name: nil, password: nil}
-  @account %Account{
-    id: "f123dc8d-a24f-47af-ab80-91f50ad28f3c",
-    name: @create_attrs.name,
-    number: 5,
-    total: 0,
-    password_hash: "AvO1oQD7/lx3CTDv+ryw$jDrFUEi7fMe6pVGB9fIAbEooFhvBgB/zxCsX8dCKBhI"
-  }
-  @changeset_error %Ecto.Changeset{
-    action: :insert,
-    changes: %{},
-    errors: [
-      name: {"can't be blank", [validation: :required]},
-      password_hash: {"can't be blank", [validation: :required]}
-    ],
-    data: %StoneBank.Accounts.Account{},
-    valid?: false,
-    types: %{
-      id: :binary_id,
-      inserted_at: :naive_datetime,
-      name: :string,
-      number: :integer,
-      password: :string,
-      password_hash: :string,
-      total: :integer,
-      updated_at: :naive_datetime
-    }
-  }
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -44,9 +19,10 @@ defmodule StoneBankWeb.AccountControllerTest do
 
   describe "create account" do
     test "renders account when data is valid", %{conn: conn} do
-      expect(AccountsMock, :create_account, fn _name, _password -> {:ok, @account} end)
+      account = fixture(:account, insert: false)
+      expect(AccountsMock, :create_account, fn _, _ -> {:ok, account} end)
       conn = post(conn, Routes.account_path(conn, :create), @create_attrs)
-      %Account{id: id, name: name, number: number, total: total} = @account
+      %Account{id: id, name: name, number: number, total: total} = account
 
       assert %{
                "id" => ^id,
@@ -57,7 +33,12 @@ defmodule StoneBankWeb.AccountControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      expect(AccountsMock, :create_account, fn _name, _password -> {:error, @changeset_error} end)
+      changeset_error =
+        %Account{}
+        |> Changeset.change(@invalid_attrs)
+        |> Changeset.add_error(:name, "can't be blank", validation: :required)
+
+      expect(AccountsMock, :create_account, fn _, _ -> {:error, changeset_error} end)
       conn = post(conn, Routes.account_path(conn, :create), @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
